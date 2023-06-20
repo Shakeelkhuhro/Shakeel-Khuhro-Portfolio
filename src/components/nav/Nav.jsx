c6d31c0248ee4548d5ffd13bc6527aebd8946830
Shakeel Khuhro <shakeelahmedkhuhro5454@gmail.com>
2023-06-21 00:45:17 +0500

    minor changes

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,31 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./nav.css";
 import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineUserSwitch } from "react-icons/ai";
 import { HiOutlineBookOpen } from "react-icons/hi";
 import { RiServiceLine } from "react-icons/ri";
 import { TbMessage } from "react-icons/tb";
-// hook
-import { useState } from "react";;
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState("#");
 
   return (
     <nav>
-      <a href="#" className={activeNav === "#" ? "active" : ""}>
+      <a href="/" className={activeNav === "#" ? "active" : ""}>
         <AiOutlineHome />
       </a>
-      <a href="#about" onClick={()=>setActiveNav('#about')} className={activeNav === "#about" ? "active" : ""}>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === "#about" ? "active" : ""}>
         <AiOutlineUserSwitch />
       </a>
-      <a href="#testimonials" onClick={()=>setActiveNav('#testimonials')} className={activeNav === "#testimonials" ? "active" : ""}>
+      <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className={activeNav === "#testimonials" ? "active" : ""}>
         <HiOutlineBookOpen />
       </a>
-      <a href="#services" onClick={()=>setActiveNav('#services')} className={activeNav === "#services" ? "active" : ""}>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === "#services" ? "active" : ""}>
         <RiServiceLine />
       </a>
-      <a href="#contact" onClick={()=>setActiveNav('#contact')} className={activeNav === "#contact" ? "active" : ""}>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === "#contact" ? "active" : ""}>
         <TbMessage />
       </a>
     </nav>
--- src/components/testimonails/Testimonials.jsx
+++ src/components/testimonails/Testimonials.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./testimonials.css";
 import AVTR1 from "../../assets/avatar1.jfif";
-import AVTR2 from "../../assets/avatar2.jpg";
-import AVTR3 from "../../assets/avatar3.jpg";
-import AVTR4 from "../../assets/avatar4.jpg";
+// import AVTR2 from "../../assets/avatar2.jpg";
+// import AVTR3 from "../../assets/avatar3.jpg";
+// import AVTR4 from "../../assets/avatar4.jpg";
 // Swiper core and required modules
 import { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
